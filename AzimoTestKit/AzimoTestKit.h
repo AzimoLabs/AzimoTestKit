f5dcce63cfec79e666a6b448e8bb73a57f3b521f
Mateusz Kuźnik <matkuznik@gmail.com>
2017-04-10 12:39:51 +0200

    Initial Commit

---
+++ AzimoTestKit/AzimoTestKit.h
@@ -0,0 +1,19 @@
+//
+//  AzimoTestKit.h
+//  AzimoTestKit
+//
+//  Created by Mateusz Kuznik on 10/04/2017.
+//  Copyright © 2017 Azimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AzimoTestKit.
+FOUNDATION_EXPORT double AzimoTestKitVersionNumber;
+
+//! Project version string for AzimoTestKit.
+FOUNDATION_EXPORT const unsigned char AzimoTestKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AzimoTestKit/PublicHeader.h>
+
+

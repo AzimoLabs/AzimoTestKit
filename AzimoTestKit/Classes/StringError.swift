213457686655b8ce19ff84968c93a11436aa2d9b
Mateusz Kuźnik <matkuznik@gmail.com>
2017-11-17 12:42:19 +0100

    extended String to Error protocol

---
+++ AzimoTestKit/Classes/StringError.swift
@@ -0,0 +1,11 @@
+//
+//  StringError.swift
+//  AzimoTestKit
+//
+//  Created by Mateusz Kuznik on 17/11/2017.
+//  Copyright © 2017 Azimo. All rights reserved.
+//
+
+import Foundation
+
+extension String: Error {}
